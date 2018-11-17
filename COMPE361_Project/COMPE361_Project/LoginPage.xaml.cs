157b7038988e11d0ce318492606cb41d49914408
drowzee001 <drowzee001@hotmail.com>
2018-11-17 12:13:09 -0800

    Skeleton
    Login Page skeleton completed.
    Home Page work in progress.

---
+++ COMPE361_Project/COMPE361_Project/LoginPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace COMPE361_Project
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            this.InitializeComponent();
+        }
+        private void Login(object sender, RoutedEventArgs e)
+        {
+            if (Password.Password == "password")
+            {
+                this.Frame.Navigate(typeof(HomePage));
+            }
+            else
+            {
+                error.Text = "Incorrect Password";
+            }
+        }
+        private void EnterPress(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter) this.Login(sender, e);
+        }
+    }
+}
