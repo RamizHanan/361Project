8621c24fab3e312c0a20e63b57403d2af2af55de
Ramiz Hanan <ramizhanan@gmail.com>
2018-11-30 11:19:56 -0800

    Added company and employee classes

---
+++ COMPE361_Project/COMPE361_Project/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMPE361_Project
+{
+    class Employee
+    {
+        string FirstName { get; }
+        string LastName { get; }
+        string EmailAddress { get; }
+        string CellNumber { get;  }
+        string EmployeeID { get;  }
+        bool IsAdmin { get; }
+        string UserName { get; }
+        protected string Password { get; }
+        DateTime LastClockIn;
+        DateTime LastClockOut;
+    }
+}
