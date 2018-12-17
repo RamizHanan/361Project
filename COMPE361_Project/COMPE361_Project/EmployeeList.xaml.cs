02d1f543d698e3b0fac6838ada004705031e0186
Donovan Rowzee <drowzee001@hotmail.com>
2018-12-16 22:21:20 -0800

    Unified 3 Branches

---
+++ COMPE361_Project/COMPE361_Project/EmployeeList.xaml.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Collections.ObjectModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace COMPE361_Project
+{
+    public sealed partial class EmployeeList : Page
+    {
+        public EmployeeList()
+        {
+            this.InitializeComponent();
+            newFunction();
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            var currentEmployee = (ProgramParams)e.Parameter;
+
+            activeEmployee = currentEmployee.FoundEmployee;
+
+
+
+            //Schedule.Items.Add(new ListViewItem { Content =  });
+        }
+
+        public async void newFunction()
+        {
+            //get json into string
+            employeeFile = await storageFolder.CreateFileAsync("testEmployeeFileWrite.json", Windows.Storage.CreationCollisionOption.OpenIfExists);
+            string newFile = await Windows.Storage.FileIO.ReadTextAsync(employeeFile);
+            JObject json = JObject.Parse(newFile);
+
+            //access employees
+            JArray employeeList = (JArray)json["employees"];
+
+            List<string> employees = JsonConvert.DeserializeObject<List<string>>(employeeList.ToString());
+
+            for (int i = 0; i < employees.Count; i++)
+            {
+                //Employees.Items.Add(new ListViewItem { Content = (string)json[employees[i]]["FirstName"] + " " + (string)json[employees[i]]["LastName"] });
+                Employees.Items.Add(new ListViewItem { Content = employees[i] });
+            }
+
+        }
+
+        Employee activeEmployee = new Employee();
+
+
+
+        //Create temporary files
+        Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+
+        Windows.Storage.StorageFile employeeFile;
+        public bool employeeExists = true;
+
+        public async void AddEmployee(string username, Employee newEmployee)
+        {
+            //Check if employee already exists
+            CheckIfEmployeeExists(username);
+            //If employee doesn't exist
+            if (!employeeExists)
+            {
+                //get json into string
+                employeeFile = await storageFolder.CreateFileAsync("testEmployeeFileWrite.json", Windows.Storage.CreationCollisionOption.OpenIfExists);
+                string newFile = await Windows.Storage.FileIO.ReadTextAsync(employeeFile);
+                JObject json = JObject.Parse(newFile);
+
+                //access employees
+                JArray employeeList = (JArray)json["employees"];
+
+                //add employee email to list
+                employeeList.Add(username);
+
+                //update json
+                await Windows.Storage.FileIO.WriteTextAsync(employeeFile, json.ToString());
+
+                //Make JSON string from dictionary
+                string newEmployeeJSON = CreateEmployeeJSON(username, newEmployee);
+                //Write to employee JSON
+                WriteToJSON(newEmployeeJSON);
+
+                //**Write Employee Successfully Added**
+                TestStatus.Text = "SUCCESS - EMPLOYEE ADDED";
+
+            }
+            //If employee already exists
+            else
+                TestStatus.Text = "ERROR - EMPLOYEE ALREADY EXISTS";
+        }
+
+        public async void WriteToJSON(string newJSON)
+        {
+            employeeFile = await storageFolder.CreateFileAsync("testEmployeeFileWrite.json", Windows.Storage.CreationCollisionOption.OpenIfExists);
+            string newFile = await Windows.Storage.FileIO.ReadTextAsync(employeeFile);
+            if (newFile == "")
+                newFile = newJSON;
+            else
+            {
+                newFile = newFile.Remove(newFile.Length - 1, 1);
+                newJSON = newJSON.Remove(0, 1);
+                newJSON = newJSON.Remove(newJSON.Length - 1, 1);
+                newFile = newFile + ',' + newJSON + '}';
+            }
+            await Windows.Storage.FileIO.WriteTextAsync(employeeFile, newFile);
+        }
+
+        public async void CheckIfEmployeeExists(string username)
+        {
+            ReadFile();
+            string employeeList = await Windows.Storage.FileIO.ReadTextAsync(employeeFile);
+
+            //Check for empty file
+            if (employeeList == "") employeeExists = false;
+            else
+            {
+
+                //Check if employee already exists
+                JObject employeeChecker = JObject.Parse(employeeList);
+
+
+
+                //If employee does not exist
+                try
+                {
+                    if (employeeChecker[username] == null) employeeExists = false;
+                    else employeeExists = true;
+                }
+                catch { employeeExists = false; }
+            }
+        }
+
+        public async void ReadFile()
+        {
+            employeeFile = await storageFolder.CreateFileAsync("testEmployeeFileWrite.json", Windows.Storage.CreationCollisionOption.OpenIfExists);
+            //employeeFile = await storageFolder.GetFileAsync("testEmployeeFileWrite.json");
+        }
+
+        public string CreateEmployeeJSON(string username, Employee newEmployee)
+        {
+            //Create employee dictionary
+            Dictionary<string, Employee> tempEmployee = new Dictionary<string, Employee>
+                {
+                    { username, newEmployee }
+                };
+
+
+            //Make JSON string from dictionary
+            return JsonConvert.SerializeObject(tempEmployee, Formatting.Indented);
+        }
+        public async void UpdateEmployee(string email)
+        {
+
+            //get json into string
+            employeeFile = await storageFolder.CreateFileAsync("testEmployeeFileWrite.json", Windows.Storage.CreationCollisionOption.OpenIfExists);
+            string newFile = await Windows.Storage.FileIO.ReadTextAsync(employeeFile);
+            JObject json = JObject.Parse(newFile);
+
+            //access employees
+            json[email]["FirstName"] = FirstName.Text;
+
+            //update json
+            await Windows.Storage.FileIO.WriteTextAsync(employeeFile, json.ToString());
+        }
+        public Employee CreateEmployee()
+        {
+            //userName is email address since that is how we login
+            Employee employee = new Employee();
+            employee.FirstName = FirstName.Text;
+            employee.LastName = LastName.Text;
+            employee.CellNumber = PhoneNumber.Text;
+            employee.EmailAddress = Username.Text.ToLower(); //keep it lowercase cuz email
+            employee.Address = Address.Text;
+            // employee.employeeType = EmployeeType.Text;
+            return employee;
+        }
+
+        //Create new employee
+        private async void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Employee employee = CreateEmployee();
+            AddEmployee(Username.Text, employee);
+        }
+
+        private async void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateEmployee(Username.Text);
+        }
+
+        //Loads employee data
+        private async void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            ReadFile();
+            CheckIfEmployeeExists(Username.Text);
+            if (!employeeExists) TestStatus.Text = "ERROR - EMPLOYEE DOES NOT EXIST";
+            else
+            {
+                try
+                {
+                    string employeeListString = await Windows.Storage.FileIO.ReadTextAsync(employeeFile);
+                    JObject employeeList = JObject.Parse(employeeListString);
+
+                    //NEW
+                    JObject employeeTarget = (JObject)employeeList[Username.Text];
+
+                    string EmployeeJSON = employeeTarget.ToString();
+
+                    //Taken out temporarily
+                    //string EmployeeJSON = employeeList[Username.Text][Password.Text].ToString();
+
+                    Employee foundEmployee = new Employee();
+                    Newtonsoft.Json.JsonConvert.PopulateObject(EmployeeJSON, foundEmployee);
+                    FirstName.Text = foundEmployee.FirstName;
+                    LastName.Text = foundEmployee.LastName;
+                    Username.Text = foundEmployee.EmailAddress;
+                    PhoneNumber.Text = foundEmployee.CellNumber;
+                    Address.Text = foundEmployee.Address;
+                    //    EmployeeType.Text = foundEmployee.employeeType;
+                    TestStatus.Text = "SUCCESS - EMPLOYEE FOUND";
+                }
+                catch { TestStatus.Text = "ERROR - EMPLOYEE DOES NOT EXIST"; }
+            }
+        }
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBlock_SelectionChanged_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void First_Copy1_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Username_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Employees_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
--- COMPE361_Project/COMPE361_Project/LoginPage.xaml.cs
+++ COMPE361_Project/COMPE361_Project/LoginPage.xaml.cs
@@ -12,93 +12,292 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Windows.Security.Authentication.Web;
-using Windows.Web.Http;
 using Windows.Data.Json;
-using Google.Apis;
+using Windows.Storage;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using Windows.Storage.Streams;
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace COMPE361_Project
 {
     public sealed partial class LoginPage : Page
     {
+        string employeeEmail;
+        Employee currentEmployee;
+
         public LoginPage()
         {
             InitializeComponent();
         }
-         public async void accessData(string token)
+
+        public Employee Parameter()
         {
-            var restApi = new Uri(@"https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" + token);
-            using (var client = new HttpClient())
-            {
-                var infoResult = await client.GetAsync(restApi);
-                string content = await infoResult.Content.ReadAsStringAsync();
-                status.Text = content;
-                var jsonObject = JsonObject.Parse(content);
-                //string name = jsonObject["email"].GetString();
-
-               // status.Text = "Id: " + id;
-                //status.Text = "Name: " + name;
-            }
-
+            return currentEmployee;
         }
 
-        private async void Login_Click(object sender, RoutedEventArgs e)
+        const string clientID = "581786658708-r4jimt0msgjtp77b15lonfom92ko6aeg.apps.googleusercontent.com";
+        const string redirectURI = "pw.oauth2:/oauth2redirect";
+        const string authorizationEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+        const string tokenEndpoint = "https://www.googleapis.com/oauth2/v4/token";
+        const string userInfoEndpoint = "https://www.googleapis.com/oauth2/v3/userinfo";
+
+        /// <summary>
+        /// Starts an OAuth 2.0 Authorization Request.
+        /// </summary>
+        private void Login_Click(object sender, RoutedEventArgs e)
         {
-            var googleUrl = new System.Text.StringBuilder();
-            googleUrl.Append("https://accounts.google.com/o/oauth2/auth?client_id=");
-            googleUrl.Append(Uri.EscapeDataString("386704258758-9cjrkli52jod17eu2785helpu59oda0a.apps.googleusercontent.com"));
-            googleUrl.Append("&scope=openid%20email%20profile");
-            googleUrl.Append("&redirect_uri=");
-            googleUrl.Append(Uri.EscapeDataString("urn:ietf:wg:oauth:2.0:oob:auto"));
-            googleUrl.Append("&response_type=code");
-            googleUrl.Append("&include_granted_scopes=true");
-            var startURI = new System.Uri(googleUrl.ToString());
-            var endURI = new System.Uri("https://accounts.google.com/o/oauth2/approval");
+            // Generates state and PKCE values.
+            string state = randomDataBase64url(32);
+            string code_verifier = randomDataBase64url(32);
+            string code_challenge = base64urlencodeNoPadding(sha256(code_verifier));
+            const string code_challenge_method = "S256";
 
-            try
+            // Stores the state and code_verifier values into local settings.
+            // Member variables of this class may not be present when the app is resumed with the
+            // authorization response, so LocalSettings can be used to persist any needed values.
+            ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+            localSettings.Values["state"] = state;
+            localSettings.Values["code_verifier"] = code_verifier;
+            // Creates the OAuth 2.0 authorization request.
+            string authorizationRequest = string.Format("{0}?response_type=code&scope=openid%20email&redirect_uri={1}&client_id={2}&state={3}&code_challenge={4}&code_challenge_method={5}",
+                authorizationEndpoint,
+                System.Uri.EscapeDataString(redirectURI),
+                clientID,
+                state,
+                code_challenge,
+                code_challenge_method);
+            
+            // Opens the Authorization URI in the browser.
+            var success = Windows.System.Launcher.LaunchUriAsync(new Uri(authorizationRequest));
+        }
+
+        /// <summary>
+        /// Processes the OAuth 2.0 Authorization Response
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e.Parameter is Uri)
             {
-                WebAuthenticationResult result =
-                    await WebAuthenticationBroker.AuthenticateAsync(
-                        WebAuthenticationOptions.None, startURI, endURI);
+                // Gets URI from navigation parameters.
+                Uri authorizationResponse = (Uri)e.Parameter;
+                string queryString = authorizationResponse.Query;
 
-                switch (result.ResponseStatus)
+                // Parses URI params into a dictionary
+                // ref: http://stackoverflow.com/a/11957114/72176
+                Dictionary<string, string> queryStringParams =
+                        queryString.Substring(1).Split('&')
+                             .ToDictionary(c => c.Split('=')[0],
+                                           c => Uri.UnescapeDataString(c.Split('=')[1]));
+
+                if (queryStringParams.ContainsKey("error"))
                 {
-                    case WebAuthenticationStatus.Success:
-                        var restApi = new Uri(result.ResponseData.ToString());
-                        using (var client = new HttpClient())
-                        {
-                            var infoResult = await client.GetAsync(restApi);
-                            string content = await infoResult.Content.ReadAsStringAsync();
-                            var jsonObject = JsonObject.Parse(content);
-                            string id = jsonObject["id"].GetString();
-                            string name = jsonObject["name"].GetString();
-
-                            //UserIdTextBlock.Text = "Id: " + id;
-                            status.Text = "Name: " + name;
-                        }
-
-                        break;
-                    case WebAuthenticationStatus.ErrorHttp:
-                        status.Text = "404";
-                        break;
-                    default:
-                        status.Text = "What Happened?";
-                        break;
+                    output(String.Format("OAuth authorization error: {0}.", queryStringParams["error"]));
+                    return;
                 }
+
+                if (!queryStringParams.ContainsKey("code")
+                    || !queryStringParams.ContainsKey("state"))
+                {
+                    output("Malformed authorization response. " + queryString);
+                    return;
+                }
+
+                // Gets the Authorization code & state
+                string code = queryStringParams["code"];
+                string incoming_state = queryStringParams["state"];
+
+                // Retrieves the expected 'state' value from local settings (saved when the request was made).
+                ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+                string expected_state = (String)localSettings.Values["state"];
+
+                // Compares the receieved state to the expected value, to ensure that
+                // this app made the request which resulted in authorization
+                if (incoming_state != expected_state)
+                {
+                    output(String.Format("Received request with invalid state ({0})", incoming_state));
+                    return;
+                }
+
+                // Resets expected state value to avoid a replay attack.
+                localSettings.Values["state"] = null;
+
+                // Authorization Code is now ready to use!
+               // output(Environment.NewLine + "Authorization code: " + code);
+
+                string code_verifier = (String)localSettings.Values["code_verifier"];
+                performCodeExchangeAsync(code, code_verifier);
             }
-            catch (Exception ex)
+            else
             {
-                status.Text = ex.Message;
+                Debug.WriteLine(e.Parameter);
+            }
+        }
+
+        async void performCodeExchangeAsync(string code, string code_verifier)
+        {
+            // Builds the Token request
+            string tokenRequestBody = string.Format("code={0}&redirect_uri={1}&client_id={2}&code_verifier={3}&scope=&grant_type=authorization_code",
+                code,
+                System.Uri.EscapeDataString(redirectURI),
+                clientID,
+                code_verifier
+                );
+            StringContent content = new StringContent(tokenRequestBody, Encoding.UTF8, "application/x-www-form-urlencoded");
+
+            // Performs the authorization code exchange.
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.AllowAutoRedirect = true;
+            HttpClient client = new HttpClient(handler);
+
+           // output(Environment.NewLine + "Exchanging code for tokens...");
+            HttpResponseMessage response = await client.PostAsync(tokenEndpoint, content);
+            string responseString = await response.Content.ReadAsStringAsync();
+          //  output(responseString);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                output("Authorization code exchange failed.");
+                return;
             }
 
+            // Sets the Authentication header of our HTTP client using the acquired access token.
+            JsonObject tokens = JsonObject.Parse(responseString);
+            string accessToken = tokens.GetNamedString("access_token");
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
 
+            // Makes a call to the Userinfo endpoint, and prints the results.
+            output("Making API Call to Userinfo...");
+            HttpResponseMessage userinfoResponse = client.GetAsync(userInfoEndpoint).Result;
+            string userinfoResponseContent = await userinfoResponse.Content.ReadAsStringAsync();
+
+            //Get email from JSON
+            JObject employeeInfo = JObject.Parse(userinfoResponseContent);
+            employeeEmail = (string)employeeInfo["email"];
+
+            CheckIfEmployeeExists();
+            if (employeeExists)
+            {
+                string employeeListString = await Windows.Storage.FileIO.ReadTextAsync(employeeFile);
+                JObject employeeList = JObject.Parse(employeeListString);
+
+                JObject employeeTarget = (JObject)employeeList[employeeEmail];
+                string EmployeeJSON = employeeTarget.ToString();
+                
+                var employeeParameter = new ProgramParams();
+                Employee foundEmployee = new Employee();
+                Newtonsoft.Json.JsonConvert.PopulateObject(EmployeeJSON, foundEmployee);
+                employeeParameter.FoundEmployee = foundEmployee;
+
+                this.Frame.Navigate(typeof(PayrollSystem), employeeParameter);
+            }
+            else
+            {
+                Error.Text = "Employee does not exist";
+            }
+        }
+
+        /// <summary>
+        /// Appends the given string to the on-screen log, and the debug console.
+        /// </summary>
+        /// <param name="output">string to be appended</param>
+        public void output(string output)
+        {
+           // textBoxOutput.Text = textBoxOutput.Text + output + Environment.NewLine;
+            Debug.WriteLine(output);
+        }
+
+        /// <summary>
+        /// Returns URI-safe data with a given input length.
+        /// </summary>
+        /// <param name="length">Input length (nb. output will be longer)</param>
+        /// <returns></returns>
+        public static string randomDataBase64url(uint length)
+        {
+            IBuffer buffer = CryptographicBuffer.GenerateRandom(length);
+            return base64urlencodeNoPadding(buffer);
+        }
+
+        /// <summary>
+        /// Returns the SHA256 hash of the input string.
+        /// </summary>
+        /// <param name="inputStirng"></param>
+        /// <returns></returns>
+        public static IBuffer sha256(string inputStirng)
+        {
+            HashAlgorithmProvider sha = HashAlgorithmProvider.OpenAlgorithm(HashAlgorithmNames.Sha256);
+            IBuffer buff = CryptographicBuffer.ConvertStringToBinary(inputStirng, BinaryStringEncoding.Utf8);
+            return sha.HashData(buff);
+        }
+
+        /// <summary>
+        /// Base64url no-padding encodes the given input buffer.
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <returns></returns>
+        public static string base64urlencodeNoPadding(IBuffer buffer)
+        {
+            string base64 = CryptographicBuffer.EncodeToBase64String(buffer);
+
+            // Converts base64 to base64url.
+            base64 = base64.Replace("+", "-");
+            base64 = base64.Replace("/", "_");
+            // Strips padding.
+            base64 = base64.Replace("=", "");
+
+            return base64;
         }
 
 
         private void EnterPress(object sender, KeyRoutedEventArgs e)
         {
             if (e.Key == Windows.System.VirtualKey.Enter) this.Login_Click(sender, e);
+        }
+
+
+        //Start of PASTED CODE
+
+
+        //Create temporary files
+        Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+        Windows.Storage.StorageFile employeeFile;
+        public bool employeeExists = true;
+
+        public async void CheckIfEmployeeExists()
+        {
+            ReadFile();
+            string employeeList = await Windows.Storage.FileIO.ReadTextAsync(employeeFile);
+
+            //Check for empty file
+            if (employeeList == "") employeeExists = false;
+            else
+            {
+
+                //Check if employee already exists
+                JObject employeeChecker = JObject.Parse(employeeList);
+
+                //If employee does not exist
+                try
+                {
+                    if (employeeChecker[employeeEmail] == null) employeeExists = false;
+                    else employeeExists = true;
+                }
+                catch { employeeExists = false; }
+            }
+        }
+        
+        public async void ReadFile()
+        {
+            employeeFile = await storageFolder.CreateFileAsync("testEmployeeFileWrite.json", Windows.Storage.CreationCollisionOption.OpenIfExists);
         }
     }
 }
