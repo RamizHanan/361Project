e2f9224362cebcc7a603a1ac1288cabb34a57514
Donovan Rowzee <drowzee001@hotmail.com>
2018-12-17 20:25:34 -0800

    Created GUI for Editing Schedule

--- COMPE361_Project/COMPE361_Project/EditSchedule.xaml.cs
+++ COMPE361_Project/COMPE361_Project/EditSchedule.xaml.cs
@@ -25,6 +25,44 @@ namespace COMPE361_Project
         public EditSchedule()
         {
             this.InitializeComponent();
+            for(int i = 0; i < 2400; i += 100)
+            {
+                for (int j = 1; j <= 4; j++)
+                {
+                    MenuFlyoutItem x = new MenuFlyoutItem();
+                    MenuFlyoutItem y = new MenuFlyoutItem();
+                    x.Text = (j * 15 == 60) ? $"{i + (j * 15) + 40}" : $"{i + (j * 15)}";
+                    y.Text = (j * 15 == 60) ? $"{i + (j * 15) + 40}" : $"{i + (j * 15)}";
+                    x.Click += new RoutedEventHandler(StartTimeSelect);
+                    y.Click += new RoutedEventHandler(EndTimeSelect);
+                    StartTimeOptions.Items.Add(x);
+                    EndTimeOptions.Items.Add(y);
+                }
+            }
+        }
+        private void DateSelect(CalendarDatePicker sender, CalendarDatePickerDateChangedEventArgs args)
+        {
+            dateSelected.Text = sender.Date.Value.ToString("MM.dd.yyyy");
+        }
+        private void StartTimeSelect(object sender, RoutedEventArgs e)
+        {
+            MenuFlyoutItem x = sender as MenuFlyoutItem;
+            Start.Text = x.Text;
+        }
+        private void EndTimeSelect(object sender, RoutedEventArgs e)
+        {
+            MenuFlyoutItem x = sender as MenuFlyoutItem;
+            if (Int32.Parse(x.Text) < Int32.Parse(Start.Text) || Start.Text == null)
+            {
+                Output.Text = "Cannot Schedule End Time before Start Time";
+            }
+            else
+            End.Text = x.Text;
+        }
+        private void Schedule(object sender, RoutedEventArgs e)
+        {
+            if(Start.Text == "" || End.Text == "" || dateSelected.Text == "") Output.Text = "Please Select Every Parameter";
+            else Output.Text = "Successful Schedule";
         }
     }
 }
